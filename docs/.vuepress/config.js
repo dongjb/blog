8d746af068d33be8e054ed9a4bc1c26980bb7bf1
dongjb <dongjb@seentao.com>
2020-07-24 16:57:06 +0800

    :sparkles: 添加百度统计代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,8 @@ module.exports = {
   dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+    ['script', { src: '/bd.js' }]
   ],
   theme: 'reco',
   themeConfig: {
---
+++ docs/.vuepress/public/bd.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?582f72236fdaa3fda0fcbf4820f5faf5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
